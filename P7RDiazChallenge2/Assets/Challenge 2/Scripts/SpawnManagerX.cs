5c428a668ff4c99e5155780f5272a1e0e68c3bd2
Otrebor0417 <145072312+Otrebor0417@users.noreply.github.com>
2026-03-20 14:43:23 -0700

    gnfxxngcfz

--- P7RDiazChallenge2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ P7RDiazChallenge2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -23,19 +23,16 @@ public class SpawnManagerX : MonoBehaviour
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
-        // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         int ballIndex = Random.Range(0, ballPrefabs.Length);
-        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
 
-        // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, Quaternion.identity);
 
         spawnInterval = Random.Range(0.5f, 3.0f);
 
         Invoke("SpawnRandomBall", spawnInterval);
-        Debug.Log("spawnInterval: " + spawnInterval);
     }
-
 }
+
+
